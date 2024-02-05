c3627da1b81d61650360865a78d4e5626042d8bb
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-05 12:01:33 +0100

    py

---
+++ post-checkout.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys, os, re
+
+print ""
+print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+print "~ post-checkout: Deleting all .pyc files... ~"
+print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+print ""
