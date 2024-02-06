9777eb18b11c857ded76e65d715905d4828097ae
joanpolalejandre <joan.obiols+hdw@latch.com>
2024-02-06 12:29:53 +0100

    delete py

--- post-checkout.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys, os, re
-
-print ""
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print "~ post-checkout: Deleting all .pyc files... ~"
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print ""
